c4bfe1efc1e8188bfbc2dfee5d824daf3c4405c0
arinab <a.bobrova.mail@gmail.com>
2016-06-14 09:26:52 +0800

    Posting recent updates from Fork Origin

--- CourseSessions/RTutorial/RunStudy.R
+++ CourseSessions/RTutorial/RunStudy.R
@@ -1,20 +1,27 @@
-# This is the begining of a new world.....
-# We will have some fun
-#### INITIALIZATION
-################################################################################
-rm(list = ls( ))
-getwd()
-# Let's get the data
-ProjectData = read.csv("data/Boats.csv", sep=";", dec=",", header = TRUE)
-###
-# THis is the file where i have the main report for the project 
-docfile = "doc/report.Rmd"
+# Project Name: "R tutorial"
 
-################################################################################
-#### THese are the main "parameters" of the project
+rm(list = ls()) # clean up the workspace
 
-################################################################################
-### This is where I ask RunStudy.R to "compile" my project document
-source("R/library.R")
-## Generate the report
-rmarkdown::render(docfile, quiet = TRUE )
+# When running the case on a local computer, modify this in case you saved the case in a different directory 
+# (e.g. local_directory <- "C:/user/MyDocuments" )
+# type in the Console below help(getwd) and help(setwd) for more information
+#local_directory <- paste(getwd(), "CourseSessions/Session1", sep="/")
+local_directory = getwd()
+#local_directory <- "MYDIRECTORY/CourseSessions/Session1"
+
+#cat("\n *********\n WORKING DIRECTORY IS ", local_directory, "\n PLEASE CHANGE IT IF IT IS NOT CORRECT using setwd(..) - type help(setwd) for more information \n *********")
+
+######################################################################
+
+# THESE ARE THE PROJECT PARAMETERS NEEDED TO GENERATE THE REPORT
+
+ProjectData <- read.csv(file = "data/Boats.csv", header = TRUE, sep=";")
+correlation_columns = 3:6
+
+
+# THIS IS A SPECIAL CASE PROJECT: IT ONLY HAS SLIDES. IT DOES NOT USE ANY REPORT, DATA, AND WEB APPLICATION
+
+source(paste(local_directory,"R/library.R", sep="/"))
+
+rmarkdown::render("doc/rtutorialdoc.Rmd", quiet = TRUE )
+
